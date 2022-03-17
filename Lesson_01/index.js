de069b8df14d8cc02689af40e50c835272131328
Нурсултан <nursultanerzhan9301@gmail.com>
2022-03-17 23:24:17 +0600

    First commit

---
+++ Lesson_01/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = "keyBoard";
+let productCount = 150;
+const productCategory = 'device';
+let productPrice = 50;
+
+console.log('productName: ', productName);
+console.log('totalAmount: ', productCount * productPrice);
