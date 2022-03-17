b4ec0283198d2c4c59e1f83417221892dd4f1e41
Нурсултан <nursultanerzhan9301@gmail.com>
2022-03-17 23:42:00 +0600

    second commit 2

--- Lesson_01/index.js
+++ Lesson_01/index.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const productName = "keyBoard";
-let productCount = 150;
-const productCategory = 'device';
-let productPrice = 50;
+{
+    const productName = "keyBoard";
+    const productCount = 150;
+    const productCategory = 'device';
+    const productPrice = 50;
 
-console.log('productName: ', productName);
-console.log('totalAmount: ', productCount * productPrice);
+    console.log('productName: ', productName);
+    console.log('totalAmount: ', productCount * productPrice);
+}
+
+{
+    const productName = "Ручка";
+    const productCount = 500;
+    const productCategory = 'канцелярские';
+    const productPrice = 9;
+
+    console.log('productName: ', productName);
+    console.log('totalAmount: ', productCount * productPrice);
+}
