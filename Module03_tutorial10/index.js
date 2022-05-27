f1064db1caf7a8203250dc64f2185f3d0d8c97fe
Нурсултан <nursultanerzhan9301@gmail.com>
2022-05-28 00:34:04 +0600

    card mask

--- Module03_tutorial10/index.js
+++ Module03_tutorial10/index.js
@@ -1,28 +1,28 @@
- import {el, mount} from './node_modules/redom/dist/redom.es.js';
+import { el, mount } from './node_modules/redom/dist/redom.es.js';
 
-const wrapper = 
-el('div', {className: 'wrapper'}, 
-    el('div',{className:'card'}, 
-        el('p',{className:'secure'},'Secure Checkout'),
-        el('div',{className:'credit-card'},
-            el('span',{className:'card__number'},'xxxx xxxx xxxx xxxx'),
-            el('div',{className:'card__personal'},
-                el('span', {className:'card__name'},'John Doe'),
-                el('span',{className:'card__date'},'04/24'))),
-        el('form',{className:'form', id:'form'}, 
-            el('div',{className:'form__input-wrap form__input-wrap_holder'},
-                el('label', {className:'form__label form__holder-label'}, 'Card Holder'),
-                el('input', {className:'input input__holder', type:'text'})),
-            el('div', {className:'form__input-wrap form__input-wrap_number'},
-                el('label',{className:'form__label form__number-label'},'Card Number'),
-                el('input',{className:'input input__number', id:'cardNumber'})),
-            el('div',{className:'form__input-wrap form__input-wrap_date'},
-                el('label',{className:'form__label form__date-label'},'Card Expiry'),
-                el('input',{className:'input input__date', type:'text'})),
-            el('div',{className:'form__input-wrap form__input-wrap_cvv'},
-                el('label',{className:'form__label form__cvv-label'},'CVV'),
-                el('input',{className:'input input__cvv', type:'text'})),
-            el('button',{className:'form__button'},'CHECK OUT'))));
+const wrapper =
+    el('div', { className: 'wrapper' },
+        el('div', { className: 'card' },
+            el('p', { className: 'secure' }, 'Secure Checkout'),
+            el('div', { className: 'credit-card' },
+                el('span', { className: 'card__number' }, ''),
+                el('div', { className: 'card__personal' },
+                    el('span', { className: 'card__name' }, 'John Doe'),
+                    el('span', { className: 'card__date' }, '04/24'))),
+            el('form', { className: 'form', id: 'form' },
+                el('div', { className: 'form__input-wrap form__input-wrap_holder' },
+                    el('label', { className: 'form__label form__holder-label' }, 'Card Holder'),
+                    el('input', { className: 'input input__holder', type: 'text' })),
+                el('div', { className: 'form__input-wrap form__input-wrap_number' },
+                    el('label', { className: 'form__label form__number-label' }, 'Card Number'),
+                    el('input', { className: 'input input__number', id: 'cardNumber' })),
+                el('div', { className: 'form__input-wrap form__input-wrap_date' },
+                    el('label', { className: 'form__label form__date-label' }, 'Card Expiry'),
+                    el('input', { className: 'input input__date', type: 'text' })),
+                el('div', { className: 'form__input-wrap form__input-wrap_cvv' },
+                    el('label', { className: 'form__label form__cvv-label' }, 'CVV'),
+                    el('input', { className: 'input input__cvv', type: 'text' })),
+                el('button', { className: 'form__button' }, 'CHECK OUT'))));
 
 mount(document.body, wrapper);
 
@@ -34,7 +34,41 @@ inputHolder.addEventListener('input', () => {
     cardName.textContent = inputHolder.value;
 });
 
+const setSpace = numbers => {
+    const length = numbers.length;
+    if (length < 5)
+        return numbers;
+
+    let nums = numbers;
+    let temp = [];
+    const b = length / 4;
+
+    for (let i = 0; i < b; i++) {
+        temp.push(nums.slice(0, 4));
+        nums = nums.slice(4, nums.length);
+    }
+
+    let result = '';
+    temp.forEach(item => {
+        result += item + ' ';
+    });
+    result = result.slice(0, result.length - 1);
+    return result;
+};
+
 const inputNumber = document.querySelector('.input__number');
 inputNumber.addEventListener('input', () => {
-    cardNumber.textContent = inputNumber.value; 
+    if (inputNumber.value.length > 19) {
+        inputNumber.value = inputNumber.value.slice(0,19);
+    }
+    else {
+        let a = inputNumber.value.replace(/\D/g, '');
+        a = a.replace(/\s/g, '');
+        inputNumber.value = cardNumber.textContent = setSpace(a);
+    }
+
 });
+
+
+
+// console.log(setSpace('1278989854535138'));
